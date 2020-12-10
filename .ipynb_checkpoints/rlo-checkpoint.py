9adc3e0fd9d6fa519a26004f910376371b1ae80c
Filipe Duarte <filipe_pb_duarte@hotmail.com>
2020-12-10 13:21:08 -0300

    ajustando rlo.py

--- .ipynb_checkpoints/rlo-checkpoint.py
+++ .ipynb_checkpoints/rlo-checkpoint.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy import stats
 from sklearn.linear_model import Perceptron
-from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import BaseEnsemble
 
